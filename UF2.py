060fc7350ee12a6dfc6460e218178e4129bc4e26
Cento [NES] <cento@riudaura.org>
2024-11-11 11:57:04 +0100

    update

---
+++ UF2.py
@@ -0,0 +1,43 @@
+#Aqui trobaras exersisi de la UF2
+
+#classes i objectes 
+
+#    Classe Cotxe: Crea una classe Cotxe amb els atributs marca, model i any. Defineix un mètode per mostrar aquesta informació.
+
+
+
+#    Classe Rectangle: Defineix una classe Rectangle amb base i alçada, i un mètode per calcular l'àrea.
+
+
+
+#    Classe Llibre: Crea una classe Llibre amb atributs títol, autor i pàgines. Inclou un mètode que permeti obtenir el títol i l'autor en una sola cadena.
+
+
+
+#    Classe Persona amb Edat: Crea una classe Persona que tingui nom i edat. Afegeix un mètode que indiqui si la persona és més gran.
+
+
+
+#    Classe Punt: Crea una classe Punt amb atributs x i y. Afegeix un mètode per calcular la distància entre dos punts.
+
+
+
+#Tema 2: Mètodes i Encapsulament
+
+#    Encapsulament de Persona: Defineix la classe Persona amb un atribut privat nom. Afegeix mètodes per accedir i modificar el nom.
+
+
+
+#    Mètode per Incrementar Edat: A la classe Persona, crea un mètode aniversari que augmenti l'edat en 1 any.
+
+
+
+#    Encapsulament de Saldo: Crea una classe CompteBancària amb saldo privat. Afegeix mètodes per ingressar i retirar diners.
+
+
+
+#    Classe Producte amb Preu: Defineix una classe Producte amb nom i preu (privat). Afegeix mètodes per accedir i modificar el preu.
+
+
+
+#    Mètode per Mostrar Dades: En una classe Alumne amb nom i nota privats, afegeix un mètode per mostrar les dades si la nota és més gran que 5.
